3bfd51fb93ab54c4effd3120baea58cfdd5aa439
Matti Vaatto <matti.vaattovaara@student.tut.fi>
2017-03-16 21:05:44 +0200

    Tallentaa ja näyttää tekstiä tietokannasta

---
+++ node2/projekti/routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld', function(req, res, next) {
+  res.render('helloworld', { title: 'Hello world!' });
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our mongodb variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    var userPassword = req.body.userpassword;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail,
+        "password" : userPassword
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+module.exports = router;
+
